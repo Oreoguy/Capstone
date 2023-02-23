97e07eee5af3dcd1de54559e401cd8172ba4ed3b
Shivam <shivamtirath@gmail.com>
2023-02-23 15:20:17 +0530

    addition basic functionality

---
+++ add.py
@@ -0,0 +1,3 @@
+def add(a, b):
+    z = a + b
+    return z
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # initialise calculator file which will include functionalities
 # this is executable py file only
+import add
 
 width = 50
 print("Welcome to Calculator App".center(width,"-"))
+
+x = int(input("\n\nEnter first number: "))
+y = int(input("Enter second number: "))
+
+operation = str(input("\nEnter operation to perform: "))
+
+if operation == "add":
+    result = add.add(x,y)
+
+print("\nYour result is: ", result)
