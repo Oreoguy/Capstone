50bbfe7d1bb3c25e0f1c2c9f7747aac22e153106
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2023-03-13 11:50:06 +0530

    All changes

--- Script1.js
+++ Script1.js
@@ -1,8 +1,18 @@
-
+// program to print the text
+// variable a cannot be used here
 function greet() {
+    let a = 'hello';
 
-    var a = 'hello';
-    console.log(a);
+    // variable b cannot be used here
+    if(a == 'hello'){
+        // variable b can be used here
+        let b = 'world';
+        console.log(a + ' ' + b);
+    }
+
+     // variable b cannot be used here
+    console.log(a + ' ' + b); // error
 }
+// variable a cannot be used here
 
-greet(); // hello
+greet();
--- script2.js
+++ script2.js
@@ -1,1 +1,10 @@
-console.log("New js file")
+// program to print text
+// variable a cannot be used here
+function greet() {
+    // variable a can be used here
+    var a = 'hello';
+    console.log(a);
+}
+// variable a cannot be used here
+
+greet(); // hello
